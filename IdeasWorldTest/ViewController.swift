7e01921aa2996793131813f1d3915e80e0e00a9f
Nikita Nikitin <nikita.nikitin@natlex.ru>
2021-01-27 18:36:23 +0300

    First init

---
+++ IdeasWorldTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  IdeasWorldTest
+//
+//  Created by Nikitin Nikita on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
