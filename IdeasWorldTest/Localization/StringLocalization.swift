b5d56bb8218677f1306ea605ae3ba99cbd6cab6d
Nikita Nikitin <nikita.nikitin@natlex.ru>
2021-02-01 20:20:45 +0300

    Localization was added.

---
+++ IdeasWorldTest/Localization/StringLocalization.swift
@@ -0,0 +1,46 @@
+//
+//  StringLocalization.swift
+//  IdeasWorldTest
+//
+//  Created by Nikitin Nikita on 01.02.2021.
+//
+
+import Foundation
+
+class StringLocalization {
+    var repositoryName: String {
+        NSLocalizedString("repositoryName", comment: "")
+    }
+
+    var repositoryDescription: String {
+        NSLocalizedString("repositoryDescription", comment: "")
+    }
+
+    var userEmail: String {
+        NSLocalizedString("userEmail", comment: "")
+    }
+
+    var username: String {
+        NSLocalizedString("username", comment: "")
+    }
+
+    var searchRepositories: String {
+        NSLocalizedString("searchRepositories", comment: "")
+    }
+
+    var favouriteRepositories: String {
+        NSLocalizedString("favouriteRepositories", comment: "")
+    }
+
+    var doNotHaveRepositories: String {
+        NSLocalizedString("doNotHaveRepositories", comment: "")
+    }
+
+    var searchTitle: String {
+        NSLocalizedString("searchTitle", comment: "")
+    }
+
+    var repositoryInformationTitle: String {
+        NSLocalizedString("repositoryInformationTitle", comment: "")
+    }
+}
